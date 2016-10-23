824e1b02267206a1f078228cb8ee6be50f232c9a
David Guttman <david@davidguttman.com>
2016-10-23 11:43:24 -0700

    added link to fullstackreact

--- client/views/header.js
+++ client/views/header.js
@@ -1,8 +1,19 @@
 const html = require('choo/html')
+var ga = require('ga-browserify')
+var tracker = ga('UA-8301156-3')
 
 module.exports = function () {
   return html`
     <div>
+      <div class='promo gray i'>
+        Pssst... if you'd like to learn how to build apps like this, check out my
+        <a
+          class='gray dim'
+          onclick=${logClick}
+          href='https://fullstackreact.com/?via=overwatchcountersguide' target='_blank'>
+          book on React</a>.
+      </div>
+
       <div class='fl w-25 pa2'></div>
       <div class="fl w-75 pa2">
         <p class="f6">
@@ -31,4 +42,8 @@ module.exports = function () {
 
 function on (path) {
   return window.location.pathname === path ? 'selected' : ''
+}
+
+function logClick (evt) {
+  tracker._trackEvent('misc', 'click', 'reactbook', 1)
 }
