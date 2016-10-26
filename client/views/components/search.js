c8f3a0019366ac504451644d3f987306d5bf7ee6
David Guttman <david@davidguttman.com>
2016-10-26 14:33:04 -0700

    misc

--- client/views/components/search.js
+++ client/views/components/search.js
@@ -24,31 +24,3 @@ module.exports = function (term, cb) {
     </div>
   `
 }
-
-
-// module.exports = function (state, prev, send) {
-//   var term = state.searchTerm
-//
-//   return html`
-//     <div>
-//       <input
-//         type='text'
-//         value=${state.searchTerm}
-//         placeholder='Search'
-//         onkeyup=${search}
-//         class='${term ? 'light-gray' : 'gray'} bg-dark-gray ba pa2 mt3' />
-//
-//       <span>
-//         ${!term ? '' : html`
-//           <a
-//             onclick=${(e) => send('setSearchTerm', '')}
-//             class='f5 dim white underline pa2'>
-//             Clear
-//           </a>
-//         `}
-//       </span>
-//     </div>
-//   `
-//
-//   function search (evt) { send('setSearchTerm', evt.target.value) }
-// }
